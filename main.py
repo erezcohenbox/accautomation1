2066deb24e054ea34c33de403b82c9ddc152caa7
Erez Cohen <erez.cohen.box@gmail.com>
2022-06-12 10:37:23 +0300

    change to main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class bcolors:
     OK = '\033[92m'      #GREEN
     WARNING = '\033[93m' #YELLOW
     FAIL = '\033[91m'    #RED
-    RESET = '\033[0m'
+    RESET = '\033[0m'    #RESET
 
 def main():
     main_menu()
