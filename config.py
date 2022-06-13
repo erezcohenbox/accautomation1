05d82acb016e481b5f61996924e8b593389ff527
Erez Cohen <erez.cohen.box@gmail.com>
2022-06-13 12:35:04 +0300

    updated config.py file

--- config.py
+++ config.py
@@ -6,23 +6,31 @@ class bcolors:
     FAIL = '\033[91m'    #RED
     RESET = '\033[0m'
 
-servers = {
-    "host": "10.1.16.55",
-    "user": "root",
-    "passwd": "ANX406729",
-    "db": "write-math",
-}
 
 #https://docs.python.org/3/library/configparser.html
 config = configparser.ConfigParser()
-config['DEFAULT'] = {'host': '10.1.16.55',
+config['SERVER_1'] = {'host': 'xxx.xxx.xxx.xxx',
                     'user': 'aeonixadmin',
                     'password': 'anx'}
 
-#inifile = open("configfile.ini", 'w')
-#for key, value in records.items():
+config['SERVER_2'] = {'host': 'xxx.xxx.xxx.xxx',
+                    'user': 'aeonixadmin',
+                    'password': 'anx'}
 
-config['DEFAULT']['ForwardX11'] = 'yes'
+config['SERVER_3'] = {'host': 'xxx.xxx.xxx.xxx',
+                    'user': 'aeonixadmin',
+                    'password': 'anx'}
+
+config['SERVER_3'] = {'host': 'xxx.xxx.xxx.xxx',
+                    'user': 'aeonixadmin',
+                    'password': 'anx'}
+
+config['SERVER_4'] = {'host': 'xxx.xxx.xxx.xxx',
+                    'user': 'aeonixadmin',
+                    'password': 'anx'}
+
+#config['SERVER_1']['password'] = 'ANX406729'
+
 with open('configfile.ini', 'w') as configfile:
     config.write(configfile)
 
