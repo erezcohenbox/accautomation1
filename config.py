464452ff623fae69010f16b02cf356c9435a723c
Erez Cohen <erez.cohen.box@gmail.com>
2022-06-20 15:01:19 +0300

    moving from configParser to configObj_stage1

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import configparser
 import os
 import ipaddress
-
+from configobj import ConfigObj
 
 
 class bcolors:
@@ -42,7 +42,6 @@ def setconfigfile():
     #https://docs.python.org/3/library/configparser.html
     config = configparser.ConfigParser()
     fileoptions = checkconfigstatus()
-    #print(fileoptions)
     
     prompt = f"""
     Aeonix Contact Center environment configuration Menu
@@ -59,11 +58,10 @@ def setconfigfile():
     
     if choice in ["1"]:
         if 'add' in fileoptions: 
-            sectioncount=readsections()
-            addsection(sectioncount)
+            addsection()
     elif choice in ["2"]:
         if 'edit' in fileoptions:
-            print('you can edit')
+            editsection()
     elif choice in ["3"]:
         if 'delete' in fileoptions:
             deletesection()
@@ -76,15 +74,14 @@ def setconfigfile():
     setconfigfile()
 
 
-def readsections():
-    config = configparser.ConfigParser()
-    config.read('configfile.ini')
-    return(len(config.sections()))
+def countsections():
+    config = ConfigObj('configfile.ini')
+    return(len(config.sections))
 
-def addsection(sectioncount):
-    sectioncount += 1
-    print(bcolors.WARNING + 'adding [SERVICE_' + str(sectioncount) +'] section' + bcolors.RESET)
-    #print(sectioncount)
+def addsection():
+    config = ConfigObj('configfile.ini')
+    servernumber = len(config.sections) + 1
+    print(bcolors.WARNING + 'adding [SERVICE_' + str(servernumber) +'] section' + bcolors.RESET)
     
     try:
         host = input(bcolors.INFO + 'host = '+ bcolors.RESET )
@@ -106,65 +103,88 @@ def addsection(sectioncount):
     
     choice = input(bcolors.WARNING + 'add server to configuration? [YES] ' + bcolors.RESET)
     if choice in ["YES"]:
-        config = configparser.ConfigParser()
-        with open("configfile.ini") as configfile:
-            config.read_file(configfile)
-        config['SERVER_' + str(sectioncount)] = {'host': host,
-                        'user': username,
-                        'password': password}
-        with open("configfile.ini", "w") as configfile:
-            config.write(configfile)     
-            print(bcolors.INFO + 'done.' + bcolors.RESET)
+        
+        config = ConfigObj('configfile.ini')
+        config['SERVER_' + str(servernumber)] = {}
+        config['SERVER_' + str(servernumber)]['host'] = host
+        config['SERVER_' + str(servernumber)]['user'] = username
+        config['SERVER_' + str(servernumber)]['password'] = password
+        config.write()
+        print(bcolors.OK + 'done.' + bcolors.RESET)
+
+
+def editsection():
+    config = ConfigObj('configfile.ini')
+    sectioncount = len(config.sections)
+    print(bcolors.WARNING + 'which one of the servers you wish to edit?' + bcolors.RESET)
+    print(bcolors.INFO + ', '.join(config.sections) + bcolors.RESET)
+    servernumber = input(bcolors.WARNING + 'please type the SERVER number: ' + bcolors.RESET)
+   
+    try:
+        host = input(bcolors.INFO + 'host = '+ bcolors.RESET )
+        ip = ipaddress.ip_address(host)
+        #print(f'{ip} is correct. Version: IPv{ip.version}')
+    except ValueError:
+        print(bcolors.FAIL + 'invalid ip address...' + bcolors.RESET)
+        return()
+
+    username = input(bcolors.INFO + 'username = '+ bcolors.RESET)
+    if " " in username or username =="":
+        print(bcolors.FAIL + 'invalid username...' + bcolors.RESET)
+        return()
+    
+    password = input(bcolors.INFO + 'password = '+ bcolors.RESET)
+    if " " in password or password =="":
+        print(bcolors.FAIL + 'invalid password...' + bcolors.RESET)
+        return()
+    
+    choice = input(bcolors.WARNING + 'edit the configuration? [YES] ' + bcolors.RESET)
+    if choice in ["YES"]:
+        
+        config = ConfigObj('configfile.ini')
+        config['SERVER_' + str(servernumber)] = {}
+        config['SERVER_' + str(servernumber)]['host'] = host
+        config['SERVER_' + str(servernumber)]['user'] = username
+        config['SERVER_' + str(servernumber)]['password'] = password
+        config.write()
+        print(bcolors.OK + 'done.' + bcolors.RESET)    
 
 
 def deletesection():
-    config = configparser.ConfigParser()
-    with open('configfile.ini', 'r') as configfile:
-        config.read_file(configfile)
-    #print(config.items(1))
+    config = ConfigObj('configfile.ini')
     print(bcolors.WARNING + 'which one of the servers you wish to delete?' + bcolors.RESET)
-    print(bcolors.INFO + ', '.join(config.sections()) + bcolors.RESET)
-    choice = input(bcolors.WARNING + 'please type the SERVER number: ' + bcolors.RESET)
-    config.remove_section('SERVER_' + str(choice))
-    print(len(config.sections()))
-    
-    for sect in config.sections():
-       print('    [' + sect + ']')
-       for k,v in config.items(sect):
-            print('     {} = {}'.format(k,v))
-            #config['SERVER_' + str(sect)] = {'host': host,
-            #       'user': username,
-            #       'password': password}
-    with open('configfile.ini', 'w') as configfile:
-        config.write(configfile)
-#    for rearrange 
-#            config['SERVER_' + str(sectioncount)] = {'host': host,
-#                       'user': username,
-#                       'password': password}
-  
+    print(bcolors.INFO + ', '.join(config.sections) + bcolors.RESET)
+    servernumber = input(bcolors.WARNING + 'please type the SERVER number: ' + bcolors.RESET)
+    try:
+        config.sections.remove('SERVER_'  + str(servernumber))
+        config.write()
+        print(bcolors.OK + 'done.' + bcolors.RESET)
+        
+        #servernumber = len(config.sections)
+        #sectionlist = ', '.join(config.sections)
+        #for servers in sectionlist:
 
-
-    #with open('configfile.ini', 'r') as configfile:
-    #   print(config.read())
+        
+    except ValueError:
+        print(bcolors.FAIL + 'such server does not exist...' + bcolors.RESET)
+        return()
 
 
 def initconfigfile():
-    config = configparser.ConfigParser()
     print(bcolors.FAIL + 'initializing the configuration file...' + bcolors.RESET)
     with open('configfile.ini', 'w') as configfile:
-        config.write(configfile)
-    print(bcolors.FAIL + 'please configure the environment.' + bcolors.RESET)
+        pass
+    print(bcolors.FAIL + 'please set the environment.' + bcolors.RESET)
 
 
 def clearconfigfile():
-    config = configparser.ConfigParser()
     print(bcolors.WARNING + 'this will clear the configuration file' + bcolors.RESET)
     choice = input(bcolors.WARNING + 'are you sure? [YES] ' + bcolors.RESET)
     if choice in ["YES"]:
         with open('configfile.ini', 'w') as configfile:
-            config.write(configfile)
-        print(bcolors.INFO + 'done.' + bcolors.RESET)
-        print(bcolors.FAIL + 'please configure the environment.' + bcolors.RESET)
+            pass
+        print(bcolors.OK + 'done.' + bcolors.RESET)
+        print(bcolors.FAIL + 'please set the environment.' + bcolors.RESET)
     else:
         return()
 
@@ -172,27 +192,11 @@ def clearconfigfile():
 
 
 def overviewconfigfile():
-    config = configparser.ConfigParser()
-    try:
-        with open('configfile.ini') as configfile:
-            filesize = os.path.getsize('configfile.ini')
-            if filesize > 0:
-                config.read ('configfile.ini')
-                for sect in config.sections():
-                    print('    [' + sect + ']')
-                    for k,v in config.items(sect):
-                        print('     {} = {}'.format(k,v))
-                print()
-            else:
-                print(bcolors.FAIL + 'configuration file is empty.' + bcolors.RESET)
-                print(bcolors.FAIL + 'please configure the environment.' + bcolors.RESET)
-    except FileNotFoundError:
-        initconfigfile()
-
-
-
-#def setconfigfile():
-#    config = configparser.ConfigParser()
-#    config.read ('configfile.ini')
-#    print(config.sections())
-
+    config = ConfigObj('configfile.ini')
+    if (len(config.sections)) >= 1:
+        print(bcolors.INFO +'configuration file is not empty.' + bcolors.RESET)
+        config.get
+    else:
+        print(bcolors.FAIL + 'configuration file is empty.' + bcolors.RESET)
+        print(bcolors.FAIL + 'please set the environment.' + bcolors.RESET)
+    
