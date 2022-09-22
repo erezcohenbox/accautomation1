3642214093053f71c513b3628daf289c67c8c780
Erez Cohen <erez.cohen.box@gmail.com>
2022-09-22 11:58:54 +0300

    aeonix patch.sh for anxd

--- templates/intra/aeonix/patch.sh
+++ templates/intra/aeonix/patch.sh
@@ -2,5 +2,5 @@
 
 aeonix_params.sh -set LBL_REG_GRACE_PERIOD 0
 #\cp -f LoginBean.class /home/aeonixadmin/aeonix/webadmin/webapps/aeonix/WEB-INF/classes/redsea/conf/forms/beans/
-#\cp -f anxd /home/aeonixadmin/aeonix/local/bin/
+\cp -f anxd /home/aeonixadmin/aeonix/local/bin/
 #sudo service aeonix restartweb
